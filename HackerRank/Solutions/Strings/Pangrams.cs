248a2bb2e54bcc902841fd605c1c680461a7477d
apota <alok.pota@vauto.com>
2017-06-07 11:46:07 -0500

    Adding Strings challenge

---
+++ HackerRank/Solutions/Strings/Pangrams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://www.hackerrank.com/challenges/pangrams
+/// </summary>
+namespace HackerRank.Solutions.Strings
+{
+    public class Pangrams
+    {
+
+        public static void Run()
+        {
+
+            string s = Console.ReadLine().Trim();
+
+            HashSet<char> cset = new HashSet<char>();
+            char[] carr = s.ToLower().ToCharArray();
+            int k = carr.Length;
+
+            for (int i = 0; i < k; ++i)
+            {
+                if (Char.IsLetter(carr[i]))
+                {
+                    cset.Add(carr[i]);
+                }
+
+            }
+
+            Console.WriteLine(cset.Count == 26 ? "pangram" : "not pangram");
+        }
+    }
+}
