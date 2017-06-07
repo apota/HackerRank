3c8225b4257e5ceab0882a1d78455cbc87c3e4aa
apota <alok.pota@vauto.com>
2017-06-07 11:54:33 -0500

    Adding Strings challenge

---
+++ HackerRank/Solutions/Strings/GameOfThrones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://www.hackerrank.com/challenges/game-of-thrones/
+/// </summary>
+
+namespace HackerRank.Solutions.Strings
+{
+    public class GameOfThrones
+    {
+
+        public static void Run()
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                map[c] = 0;
+            }
+            string s = Console.ReadLine();
+            using (CharEnumerator ce = s.GetEnumerator())
+            {
+                while (ce.MoveNext())
+                {
+                    map[ce.Current] += 1;
+                }
+            }
+            Console.WriteLine(map.Where(x => x.Value % 2 != 0).Count() > 1 ? "NO" : "YES");
+
+        }
+    }
+}
