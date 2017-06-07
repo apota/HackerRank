c88ae1fb6bf388b5ad51a0912d12af7d698feaf7
apota <alok.pota@vauto.com>
2017-06-07 11:37:11 -0500

    Organizing solutions into categories

--- HackerRank/Solutions/Graph/MoonJourney.cs
+++ HackerRank/Solutions/Graph/MoonJourney.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HackerRank.Solutions
+namespace HackerRank.Solutions.Graph
 {
 
     /// <summary>
