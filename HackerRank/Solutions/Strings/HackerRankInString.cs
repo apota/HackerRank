bc955b32d4ad0323952268542eae92775458cb73
apota <alok.pota@vauto.com>
2017-06-07 11:50:06 -0500

    Adding Strings challenge

---
+++ HackerRank/Solutions/Strings/HackerRankInString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://www.hackerrank.com/challenges/hackerrank-in-a-string/
+/// </summary>
+namespace HackerRank.Solutions.Strings
+{
+    public class HackerRankInString
+    {
+
+        public static void Run()
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < q; a0++)
+            {
+                string s = Console.ReadLine();
+                Regex r = new Regex(@"h\w*a\w*c\w*k\w*e\w*r\w*r\w*a\w*n\w*k");
+                Console.WriteLine(r.Match(s).Success ? "YES" : "NO");
+            }
+        }
+    }
+}
