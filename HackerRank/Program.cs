4fc29132f579fcd895f4f4d6aba9392caea45857
apota <alok.pota@vauto.com>
2017-06-12 14:40:09 -0500

    Pulling out the DJ algo into a class

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HackerRank.Solutions;
+using HackerRank.Solutions.Graph;
 using HackerRank.Solutions.Strings;
 
 namespace HackerRank
 {
+    /// <summary>
+    /// The driver for running individual solutions..
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-//            FileInput.FetchInput();
-//            MoonJourney.Run(ConsoleInput.N, ConsoleInput.InputLines);
-                ValidString.Run();
+            //            FileInput.FetchInput();
+            //            MoonJourney.Run(ConsoleInput.N, ConsoleInput.InputLines);
+
+            //                ValidString.Run();
+            //                TwoStrings.Run();
+
+            SnakesAndLadder.Run();
+
+
+
+            //hold hold hold strong strong hold hold hold hold hold hold hold strong strong hold hold hold strong hold hold strong strong strong hold strong hold hold strong strong hold hold hold hold strong hold strong hold hold strong strong hold hold strong hold strong hold hold hold hold strong hold hold hold strong strong hold hold strong hold strong strong hold hold hold hold hold hold hold hold hold hold strong hold strong hold strong hold strong hold hold hold hold hold hold hold hold strong hold strong strong hold strong strong hold hold hold hold strong strong hold hold strong hold strong strong strong hold hold strong strong hold hold hold hold hold hold strong strong hold hold strong hold strong hold hold hold strong hold hold hold strong hold strong hold strong strong strong strong strong hold strong hold hold strong hold strong hold hold strong hold strong strong strong hold hold strong hold strong hold hold hold strong strong hold strong hold hold hold strong hold hold strong hold strong hold hold strong strong hold strong strong hold hold strong hold strong hold strong strong hold strong hold strong hold hold hold hold strong strong hold hold hold strong strong hold strong hold hold hold hold hold strong strong hold hold strong strong hold hold hold hold strong hold hold strong hold hold strong hold strong hold hold hold strong hold strong hold strong hold hold strong hold hold hold hold strong hold strong hold strong hold hold hold hold strong hold strong hold hold hold hold strong hold hold hold hold hold hold hold strong hold strong hold hold strong hold strong strong strong hold hold hold strong strong strong hold hold strong hold hold strong hold hold hold strong hold hold strong strong hold hold strong hold hold hold hold strong hold hold strong strong hold hold hold hold hold strong hold hold hold strong hold hold hold hold hold strong strong hold hold strong hold strong hold hold strong strong hold hold strong hold hold hold hold hold hold strong strong hold strong hold hold strong hold hold hold strong hold hold hold hold hold strong hold strong hold strong strong hold hold strong hold strong strong hold strong hold hold strong hold strong hold hold hold hold hold strong hold hold strong hold strong hold strong hold hold strong hold hold hold strong hold hold hold hold hold strong hold strong hold hold strong hold strong strong hold hold strong hold strong hold strong
         }
 
 
--- HackerRank/Solutions/Graph/SnakesAndLadder.cs
+++ HackerRank/Solutions/Graph/SnakesAndLadder.cs
@@ -30,9 +30,9 @@ namespace HackerRank.Solutions.Graph
                 vertices.Add("" + i);
             }
 
-            List<Edge> edges = new List<Edge>(); 
+            List<Edge> edges = new List<Edge>();
 
-           
+            //Ladders
             int ladders = Convert.ToInt32(Console.ReadLine());
             HashSet<string> specialVertices = new HashSet<string>();
             for (int i = 0; i < ladders; ++i)
@@ -41,6 +41,8 @@ namespace HackerRank.Solutions.Graph
                 specialVertices.Add(arr[0]);
                 edges.Add(new Edge(arr[0], arr[1], 0));
             }
+
+            //Snakes
             int snakes = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < snakes; ++i)
             {
@@ -49,7 +51,7 @@ namespace HackerRank.Solutions.Graph
                 edges.Add(new Edge(arr[0], arr[1], 0));
             }
 
-
+            //Fill out the rest of the board...
            
             for (int i = 1; i <= 94; ++i)
             {
@@ -68,81 +70,21 @@ namespace HackerRank.Solutions.Graph
                 }
             }
 
-
-
           
 
-            Graph g = new Graph();
-            g.edges = edges;
-            g.vertices = vertices;
+            Graph g = new Graph(edges, vertices);
 
-            g.CalculateDjkistraShortestPath();
+            Dictionary<string, double> distances = new Dictionary<string, double>();
+            Dictionary<string, string> paths = new Dictionary<string, string>();
+
+            g.Djkistra("1", "100", out distances, out paths);
+
+            Console.WriteLine(distances["100"]);
 
         }
 
     }
 
-    class Edge
-    {
-
-        public string dest;
-        public string src;
-        public int w;
-
-        public Edge(string src, string dest, int w)
-        {
-            this.src = src;
-            this.dest = dest;
-            this.w = w;
-        }
-
-    }
-
-    class Graph
-    {
-
-        public List<string> vertices;
-        public List<Edge> edges;
-
-
-        public void CalculateDjkistraShortestPath()
-        {
-            List<string> Q = new List<string>();
-            Dictionary<string, int> dist = new Dictionary<string, int>();
-            Dictionary<string, string> prev = new Dictionary<string, string>();
-
-            foreach (var v in vertices)
-            {
-                dist.Add(v, Int32.MaxValue);
-                prev.Add(v, null);
-                Q.Add(v);
-            }
-
-            dist["1"] = 0;
-            while (Q.Count != 0)
-            {
-
-                var t = dist.Where(p => Q.Contains(p.Key));
-                int min = t.Min(p => p.Value);
-                string u = t.Where(p => p.Value == min).Select(p => p.Key).First();
-                Q.Remove(u);
-               /// Debug.WriteLine("Solving " + u);
-                List<Edge> nearbyEdges = edges.Where(p => p.src == u && Q.Contains(p.dest)).ToList();
-                foreach (var edge in nearbyEdges)
-                {
-                    int alt = edge.w + dist[u];
-                    if (alt < dist[edge.dest])
-                    {
-                        dist[edge.dest] = alt;  
-                        prev[edge.dest] = u;
-                    }
-                }
-
-            }
-            Debug.WriteLine(dist["100"]  == Int32.MaxValue ? -1 : dist["100"]);
-        }
-
-    }
 
 
 }
