b396b777b163ff8e62e7f6a99df84ecd9916614f
apota <alok.pota@vauto.com>
2017-06-12 00:03:46 -0500

    Snakes and Ladders Djkistra. Some impl comments

--- HackerRank/Solutions/Graph/SnakesAndLadder.cs
+++ HackerRank/Solutions/Graph/SnakesAndLadder.cs
@@ -11,21 +11,28 @@ using System.Threading.Tasks;
 /// </summary>
 namespace HackerRank.Solutions.Graph
 {
+    /// <summary>
+    /// The snakes and ladder board consist of 100 squares (vertices). A die roll can yield numbers from 1 to 6,
+    /// hence if the dice is on vertex N, then it can go to vertex N+1, N+2, N+3... N+6, provided none of these
+    /// vertices are the mouth of the snake or the tail of a ladder. An edge would then connect two vertices with
+    /// a distance of 1 (1 die roll). The edges connecting snake and ladder vertices have distance 0. 
+    /// </summary>
     public class SnakesAndLadder
     {
 
         public static void Run()
         {
-
-
+            
+            //All squares on the snakes and ladder board (i.e. 1-100) are vertices
             List<string> vertices = new List<string>();
             for (int i = 1; i <= 100; ++i)
             {
                 vertices.Add("" + i);
             }
 
-            List<Edge> edges = new List<Edge>();
+            List<Edge> edges = new List<Edge>(); 
 
+           
             int ladders = Convert.ToInt32(Console.ReadLine());
             HashSet<string> specialVertices = new HashSet<string>();
             for (int i = 0; i < ladders; ++i)
@@ -42,6 +49,8 @@ namespace HackerRank.Solutions.Graph
                 edges.Add(new Edge(arr[0], arr[1], 0));
             }
 
+
+           
             for (int i = 1; i <= 94; ++i)
             {
                 for (int j = 1; j <= 6; ++j)
