a4f52dacaf8b2d2e62da36070896b1024a2be07e
apota <alok.pota@vauto.com>
2017-06-22 15:55:10 -0500

    Solved https://www.hackerrank.com/challenges/gem-stones

---
+++ HackerRank/Solutions/Strings/Gemstones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/// <summary>
+/// https://www.hackerrank.com/challenges/gem-stones
+/// </summary>
+public class Gemstones
+{
+
+    static int gemstones(string[] arr)
+    {
+        HashSet<char> result = new HashSet<char>();
+        List<HashSet<char>> elems = new List<HashSet<char>>();
+
+        foreach (string s in arr)
+        {
+            elems.Add(new HashSet<char>(s.ToCharArray()));
+        }
+
+        foreach (var elem in elems)
+        {
+            result.UnionWith(elem);
+        }
+        foreach (var elem in elems)
+        {
+            result.IntersectWith(elem);
+        }
+
+        return result.Count();
+
+        // Complete this function
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr = new string[n];
+        for (int arr_i = 0; arr_i < n; arr_i++)
+        {
+            arr[arr_i] = Console.ReadLine();
+        }
+        int result = gemstones(arr);
+        Console.WriteLine(result);
+    }
+}
