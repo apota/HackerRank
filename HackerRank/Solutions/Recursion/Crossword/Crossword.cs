b43f424c517481e5026da1b4ddbc23e8ab6c9a78
apota <alok.pota@vauto.com>
2017-07-08 18:30:22 -0500

    Crossword puzzle solution. Added explanation

--- HackerRank/Solutions/Recursion/Crossword/Crossword.cs
+++ HackerRank/Solutions/Recursion/Crossword/Crossword.cs
@@ -8,6 +8,18 @@ using System.Threading.Tasks;
 
 namespace HackerRank.Solutions.Recursion.Crossword
 {
+
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/crossword-puzzle
+    /// 
+    /// 
+    /// Given a grid with empty spaces (Slots). Make all permutatioms of the input word list and try to "fit" these word combos
+    /// into the slots.
+    /// 
+    /// A word fits into a slot 
+    ///     1. if its the same length as the slot
+    ///     2. if its in a slot that intersects with another slot then the word on the intersecting slot share a common character.
+    /// </summary>
     public class Crossword
     {
 
