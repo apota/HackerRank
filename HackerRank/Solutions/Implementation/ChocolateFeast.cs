8944e8fd618c5d69850bd4385003a02a9681d39f
apota <alok.pota@vauto.com>
2017-07-09 14:43:02 -0500

    More challenges

---
+++ HackerRank/Solutions/Implementation/ChocolateFeast.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Solutions.Implementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/chocolate-feast
+    /// </summary>
+    class Solution
+    {
+
+        static void Run(String[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                string[] tokens_n = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(tokens_n[0]);
+                int c = Convert.ToInt32(tokens_n[1]);
+                int m = Convert.ToInt32(tokens_n[2]);
+                double answer = 0;
+                double wrappersLeft = 0;
+                Solve(n, c, m, ref wrappersLeft, ref answer);
+                Console.WriteLine(answer);
+            }
+
+        }
+
+        private static void Solve(double moneyLeft, double cost, double wrappersPerChoco, ref double wrappersLeft, ref double answer)
+        {
+
+
+            if (moneyLeft >= cost)
+            {
+                double chocos = Math.Floor(moneyLeft / cost);
+                answer += chocos;
+                double wrapperBasedChocos = Math.Floor(chocos / wrappersPerChoco);
+                answer += wrapperBasedChocos;
+                wrappersLeft = chocos - (wrapperBasedChocos * wrappersPerChoco) + wrapperBasedChocos;
+                double dollaLeft = Math.Max(0, moneyLeft - (chocos * cost));
+                Solve(dollaLeft, cost, wrappersPerChoco, ref wrappersLeft, ref answer);
+            }
+            else
+            {
+                if (wrappersLeft >= wrappersPerChoco)
+                {
+                    double wrapperBasedChocos = Math.Floor(wrappersLeft / wrappersPerChoco);
+                    answer += wrapperBasedChocos;
+                    wrappersLeft = wrappersLeft - (wrapperBasedChocos * wrappersPerChoco) + wrapperBasedChocos;
+                    Solve(0, cost, wrappersPerChoco, ref wrappersLeft, ref answer);
+                }
+            }
+
+        }
+    }
+
+}
---
+++ HackerRank/Solutions/Recursion/RecursiveSumDigit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Solutions.Recursion
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/recursive-digit-sum
+    /// </summary>
+    class RecursiveSumDigit
+    {
+        static void Run()
+        {
+            string[] arr = Console.ReadLine().Split(' ');
+            string n = arr[0];
+            int k = Int32.Parse(arr[1]);
+            double sum = StringSum(n) * k;
+
+            SuperDigit("" + sum);
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        }
+
+        private static double StringSum(string s)
+        {
+            double sum = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                sum += Int32.Parse("" + s[i]);
+            }
+            return sum;
+        }
+
+
+        static void SuperDigit(string s)
+        {
+            if (s.Length == 1)
+            {
+                Console.WriteLine(s);
+                return;
+            }
+            //int sum  = s.ToCharArray().ToList().Sum(x => Convert.ToInt32(x)); 
+            SuperDigit("" + StringSum(s));
+
+
+        }
+    }
+}
